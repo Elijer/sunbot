5e94bb78b39ae8f7c1fafbf8b7740c421e2e576e
Jah <kennedye2@vcu.edu>
2020-12-03 23:13:07 -0500

    added config in separate file as well as initialize app and db declarations

---
+++ helpers/firebaseConfig.js
@@ -0,0 +1,10 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyA5pVfl5VrUl9qTJ3czLc8PDphxmQg9HDQ",
+    authDomain: "gyst-93a6d.firebaseapp.com",
+    databaseURL: "https://gyst-93a6d.firebaseio.com",
+    projectId: "gyst-93a6d",
+    storageBucket: "gyst-93a6d.appspot.com",
+    messagingSenderId: "526384004753",
+    appId: "1:526384004753:web:61bb880ad1422e8cf74930",
+    measurementId: "G-QC4L6XMRRX"
+  };
--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 
+import { firebaseConfig } from "./helpers/firebaseConfig";
+
 
 document.addEventListener("DOMContentLoaded", event => {
-    console.log("Hey!");
+    firebase.initializeApp(firebaseConfig);
+    var db = firebase.firestore();
+
+    console.log(db);
 });
