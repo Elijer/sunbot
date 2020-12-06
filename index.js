e991ca93c1f5d37499335030d61b748ae0608794
Jah <kennedye2@vcu.edu>
2020-12-05 22:11:44 -0500

    moved firestore test into its own file

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
-import 'firebase/auth';
 
 import { firebaseConfig } from "./helpers/firebaseConfig";
 import { handleEmulators } from "./helpers/handleEmulators";
 import { firestoreTest } from "./helpers/firestoreTest";
-import { signIn } from "./helpers/auth";
 
 
 document.addEventListener("DOMContentLoaded", event => {
@@ -13,20 +11,5 @@ document.addEventListener("DOMContentLoaded", event => {
     var db = firebase.firestore();
     handleEmulators(db);
     firestoreTest(db);
-
-
-    // EVENT LISTENERS
-    document.getElementById('login-button').addEventListener('click', function(){
-        //signIn(firebase);
-        firebase.auth().signInAnonymously()
-        .then(() => {
-            console.log("signed in");
-        })
-        .catch((error) => {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          console.log(errorCode, errorMessage);
-        })
-    });
 
 });
