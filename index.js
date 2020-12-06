cff5824bf74269f41a4771041621145f658f1f93
Jah <kennedye2@vcu.edu>
2020-12-05 22:02:57 -0500

    imported firebase auth npm package and included it in index.js

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/auth';
 
 import { firebaseConfig } from "./helpers/firebaseConfig";
 import { handleEmulators } from "./helpers/handleEmulators";
+import { firestoreTest } from "./helpers/firestoreTest";
+import { signIn } from "./helpers/auth";
 
 
 document.addEventListener("DOMContentLoaded", event => {
     firebase.initializeApp(firebaseConfig);
     var db = firebase.firestore();
     handleEmulators(db);
+    firestoreTest(db);
 
 
-    // Local Emulators Test
-    var citiesRef = db.collection("firestore-collection");
-    citiesRef.doc("firestore-document").set({
-        name: "Some document", state: "VA", country: "USA?",
-        capital: false,
-        ridesBicycles: "sure",
-        regions: ["west_coast", "norcal"]
+    // EVENT LISTENERS
+    document.getElementById('login-button').addEventListener('click', function(){
+        //signIn(firebase);
+        firebase.auth().signInAnonymously()
+        .then(() => {
+            console.log("signed in");
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+        })
     });
 
 });
