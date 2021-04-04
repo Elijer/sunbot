2144531f3dcedf8cd963d4b308dd18862ac0b614
Jah <kennedye2@vcu.edu>
2021-04-04 00:17:08 -0400

    cry incrementing is working

--- index.js
+++ index.js
@@ -23,20 +23,37 @@ var outsideOptions = [gg("outsideYes"), gg("outsideNo")];
 
 outsideOptions.forEach(function(e){
     e.addEventListener("click", function(){
+        var grey = "rgb(232, 232, 232);"
         if (e.id === "outsideYes"){
-            e.style.background = "green";
-            outsideOptions[1].style.background = "grey";
+            e.style.background = "red";
+            outsideOptions[1].style.background = "#e8e8e8";
         } else {
-            outsideOptions[1].style.background = "green";
-            outsideOptions[0].style.background = "grey";
+            outsideOptions[1].style.background = "red";
+            outsideOptions[0].style.background = "#e8e8e8";
         }
     })
 })
 
+var cryOptions = [gg("moreCry"), gg("lessCry")];
+
+cryOptions.forEach(function(e){
+
+    e.addEventListener("click", function(){
+        var grey = "rgb(232, 232, 232);"
+        if (e.id === "lessCry"){
+            if (gg("cryNumber").innerHTML > 0){
+                gg("cryNumber").innerHTML--;
+            }
+        } else {
+            gg("cryNumber").innerHTML++;
+        }
+    })
+});
+
 
 var displayDate = function(){
 
-    var dateElement = gg("date");
+    var dateElement = gg("day");
     var timeElement = gg("time");
     //dateElement.innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
     dateElement.innerHTML = moment().format('dddd, MMM Do');
