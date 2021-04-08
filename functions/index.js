5296b565d8370fc6f69d066efa7759ec00c60525
Jah <kennedye2@vcu.edu>
2021-04-07 21:05:50 -0400

    super simple test fcloud function works

--- functions/index.js
+++ functions/index.js
@@ -4,27 +4,13 @@ admin.initializeApp();
 
 const twilio = require('twilio');
 const accountSid = functions.config().twilio.sid;
-const accountSid = functions.config().twilio.token;
+//const accountSid = functions.config().twilio.token;
 
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-/* exports.test = functions.https.onCall (async(data, context) => {
-    console.log("yoyoyoyo");
-    return true;
-}); */
-
+// Test function that runs whenever entries are updated. Can be used to test twilio.
 exports.test = functions.firestore
     .document('entries/{entryId}')
     .onUpdate(event => {
-
-/*         const docId = event.params.entryId;
-
-        const data = event.data.data().name; */
-
-        console.log("heyo")
-
+        console.log("heyo stevie")
         return true;
-
     });
