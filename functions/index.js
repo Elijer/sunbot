8e7c8160185988aafd3011ca1deba6dfd5cc508a
Jah <kennedye2@vcu.edu>
2021-04-07 21:42:24 -0400

    added conditional function into the test function to see if I can query database and see if energy has already been logged

--- functions/index.js
+++ functions/index.js
@@ -2,6 +2,8 @@ const functions = require("firebase-functions");
 const admin = require('firebase-admin');
 admin.initializeApp();
 
+const moment = require('moment');
+
 const twilio = require('twilio');
 const accountSid = functions.config().twilio.sid;
 const authToken = functions.config().twilio.token;
@@ -10,21 +12,7 @@ const client = new twilio(accountSid, authToken);
 
 const twilioNumber = "+14159695584"
 const myNumber = "‪+15713024423‬";
-
-// Test function that runs whenever entries are updated. Can be used to test twilio.
-/* exports.test = functions.firestore
-    .document('entries/{entryId}')
-    .onUpdate(event => {
-        const textMessage = {
-            body: 'Moonbot: ☀️☀️☀️ Good morning Katie! It\'s time for you to log your mood. Hope you\'re day is off to a good start 💛 (Cutoff for logging morning energy is 11:59)',
-            to: myNumber,
-            from: twilioNumber
-        }
-    
-        return client.messages.create(textMessage)
-        .then(message => console.log(message.sid, 'success'))
-        .catch(err => console.log(err))
-    }); */
+const katieNumber = "+16184313089";
 
 // Morning, afternoon and evening functions
 exports.morning = functions.pubsub.schedule('5 10 * * *')
@@ -32,8 +20,8 @@ exports.morning = functions.pubsub.schedule('5 10 * * *')
   .onRun((context) => {
 
     const textMessage = {
-        body: 'Moonbot: 🌞 Good morning Katie! It\'s time for you to log your mood. Hope you\'re day is off to a good start 💛 ',
-        to: myNumber,
+        body: 'Moonbot: 🌞 Good morning Katie! It\'s time for you to log your mood. Hope you\'re day is off to a good start 💛 . Url: https://ktperiodtracker.web.app/',
+        to: katieNumber,
         from: twilioNumber
     }
 
@@ -48,8 +36,8 @@ exports.afternoon = functions.pubsub.schedule('5 16 * * *')
   .onRun((context) => {
 
     const textMessage = {
-        body: 'Moonbot: 🌳 Why it appears the sun is high in the sky. Time for you to log your afternoon energy madame. (Cutoff for logging afternoon energy is 5:59)',
-        to: myNumber,
+        body: 'Moonbot: 🌳 Why it appears the sun is high in the sky. Time for you to log your afternoon energy madame. Url: https://ktperiodtracker.web.app/',
+        to: katieNumber,
         from: twilioNumber
     }
 
@@ -59,13 +47,13 @@ exports.afternoon = functions.pubsub.schedule('5 16 * * *')
 
 });
 
-exports.evening = functions.pubsub.schedule('5 20 * * *')
+exports.evening = functions.pubsub.schedule('36 21 * * *')
   .timeZone('America/New_York') // Users can choose timezone - default is America/Los_Angeles
   .onRun((context) => {
 
     const textMessage = {
-        body: 'Moonbot: Night is falling like a soft blanket on the world 🌙. Time for you to log your evening energy, as well as any other fields you missed or should update. Hope your day was magical 💛 . (Cutoff for evening energy: 2am)',
-        to: myNumber,
+        body: 'Moonbot: Night is falling like a soft blanket on the world 🌙. Time for you to log your evening energy, as well as any other fields you missed or should update. Hope your day was magical 💛 . Url: https://ktperiodtracker.web.app/',
+        to: katieNumber,
         from: twilioNumber
     }
 
@@ -74,6 +62,37 @@ exports.evening = functions.pubsub.schedule('5 20 * * *')
     .catch(err => console.log(err))
 
 });
+
+// Test function that runs whenever entries are updated. Can be used to test twilio.
+exports.test = functions.firestore
+    .document('entries/{entryId}')
+    .onUpdate(event => {
+
+        var theMoment = moment();
+        var fsDate = theMoment.format('L').replace(/\//g, "-");
+        var docRef = admin.firestore().collection('entries').doc(fsDate)
+    
+        docRef.get().then(function(doc){
+            data = doc.data();
+
+            if (data.morningenergy){
+                console.log("no morning energy")
+            } else {
+                console.log("some morning energy");
+            }
+            
+        })
+
+        const textMessage = {
+            body: 'Moonbot: ☀️☀️☀️ Good morning Katie! It\'s time for you to log your mood. Hope you\'re day is off to a good start 💛 (Cutoff for logging morning energy is 11:59)',
+            to: myNumber,
+            from: twilioNumber
+        }
+    
+        return client.messages.create(textMessage)
+        .then(message => console.log(message.sid, 'success'))
+        .catch(err => console.log(err))
+    });
 
 // Draft Cron Function
 /* exports.scheduledFunctionCrontab = functions.pubsub.schedule('6 8 * * *')
